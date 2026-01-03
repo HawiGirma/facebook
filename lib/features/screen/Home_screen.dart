d5228c6deb34a30a0ab0519dc9a25ed2b60e6c06
HawiGirma <hawigirmamegersag@gmail.com>
2026-01-04 00:43:57 +0300

    refactor: update home screen implementation

--- lib/features/screen/Home_screen.dart
+++ lib/features/screen/Home_screen.dart
@@ -3,6 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:facebook/features/services/auth_service.dart';
 import 'package:facebook/features/screen/Login_Screen.dart';
 import 'package:facebook/features/screen/Profile_Screen.dart';
+import 'package:facebook/features/screen/Notification_Screen.dart';
+import 'package:facebook/features/screen/widgets/HomeFeedWidget.dart';
+import 'package:facebook/features/screen/widgets/VideoScreen.dart';
+import 'package:facebook/features/screen/widgets/PeopleScreen.dart';
+import 'package:facebook/features/screen/widgets/ShopScreen.dart';
+import 'package:facebook/features/screen/widgets/MenuScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,11 +20,37 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final AuthService _authService = AuthService();
   User? _user;
+  int _selectedIndex = 0; // Track selected navigation index
 
   @override
   void initState() {
     super.initState();
     _user = _authService.currentUser;
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  String _getAppBarTitle() {
+    switch (_selectedIndex) {
+      case 0:
+        return 'facebook';
+      case 1:
+        return 'Watch';
+      case 2:
+        return 'Friends';
+      case 3:
+        return 'Marketplace';
+      case 4:
+        return 'Notifications';
+      case 5:
+        return 'Menu';
+      default:
+        return 'facebook';
+    }
   }
 
   Future<void> _logout() async {
@@ -48,7 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'facebook',
+          _getAppBarTitle(),
           style: TextStyle(
             color: const Color.fromARGB(255, 2, 109, 196),
             fontFamily: 'FacebookSans',
@@ -85,180 +117,34 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Container(
-        child: Column(
-          children: [
-            // User Info Section
-            if (_user != null)
-              InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => const ProfileScreen()),
-                  );
-                },
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  color: Colors.grey[100],
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: _user!.photoURL != null
-                            ? NetworkImage(_user!.photoURL!)
-                            : null,
-                        child: _user!.photoURL == null
-                            ? Icon(Icons.person, size: 30)
-                            : null,
-                      ),
-                      SizedBox(width: 16),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              _user!.displayName ?? 'User',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            SizedBox(height: 4),
-                            Text(
-                              _user!.email ?? '',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey[600],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Icon(Icons.chevron_right, color: Colors.grey[600]),
-                    ],
-                  ),
-                ),
-              ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(width: 10),
-
-                IconButton(
-                  icon: Icon(Icons.home, color: Colors.grey, size: 30),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.video_chat, color: Colors.grey, size: 30),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.people, color: Colors.grey, size: 30),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.shop, color: Colors.grey, size: 30),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.notifications, color: Colors.grey, size: 30),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.menu, color: Colors.grey, size: 30),
-                  onPressed: () {},
-                ),
-                SizedBox(width: 10),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: const AssetImage(
-                    'assets/images/facebook.png',
-                  ),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      hintText: 'What\'s on your mind?',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.all(10),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                    ),
-                  ),
-                ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.image)),
-              ],
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    '/assets/images/facebook.png',
-                    width: 50,
-                    height: 150,
-                  ),
-                  Image.asset(
-                    '/assets/images/facebook.png',
-                    width: 50,
-                    height: 150,
-                  ),
-                  Image.asset(
-                    'assets/images/facebook.png',
-                    width: 50,
-                    height: 150,
-                  ),
-                  Image.asset(
-                    '/assets/images/facebook.png',
-                    width: 50,
-                    height: 150,
-                  ),
-                ],
-              ),
-            ),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: const AssetImage(
-                        '/assets/images/facebook.png',
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        Text('John Doe'),
-                        Text(
-                          'just now',
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w200,
-                          ),
-                        ),
-                      ],
-                    ),
-                    IconButton(onPressed: () {}, icon: Icon(Icons.more)),
-                  ],
-                ),
-                Text('lorem ipsum '),
-                Image(image: AssetImage('assets/images/facebook.png')),
-                Image(image: AssetImage('/assets/images/facebook.png')),
-              ],
-            ),
-          ],
-        ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          HomeFeedWidget(user: _user),
+          VideoScreen(),
+          PeopleScreen(),
+          ShopScreen(),
+          NotificationScreen(),
+          MenuScreen(user: _user, onLogout: _logout),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: const Color.fromARGB(255, 2, 109, 196),
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_chat), label: 'Watch'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Friends'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Marketplace'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'Menu'),
+        ],
       ),
     );
   }
